1dff2c8d020f106a3a7fdde1d30c6f97ed2eeaa5
slavov-vili <slavov.vili@gmail.com>
2018-01-02 22:19:10 +0100

    Gave up on the idea of assembling the rc files

--- bash/setup_bash.sh
+++ bash/setup_bash.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# check if the system's default bashrc was already copied
-
-
 # make links to all bash-related files
 ln -srf ./bash_profile ~/.bash_profile
 ln -srf ./bashrc       ~/.bashrc
--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# assemble the main vimrc
-# add 4 lines of space between the contents of each file
-cp  ./vimrc_base         ./vimrc
-echo -e "\n\n\n"      >> ./vimrc
-cat ./vimrc_settings  >> ./vimrc
-echo -e "\n\n\n"      >> ./vimrc
-cat ./vimrc_keymaps   >> ./vimrc
-echo -e "\n\n\n"      >> ./vimrc
-cat ./vimrc_others    >> ./vimrc
-echo -e "\n\n\n"      >> ./vimrc
-cat ./vimrc_functions >> ./vimrc
-
 # Make links to all vim-related files and folders
 ln -srf ./vimrc     ~/.vimrc
 ln -srf ./gvimrc    ~/.gvimrc
