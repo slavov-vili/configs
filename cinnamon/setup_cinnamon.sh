668515e9701467072bcbd2454fe23799cb5dbd2e
vilskin <slavov.vili@gmail.com>
2018-03-23 09:08:38 +0100

    Minor changes

---
+++ cinnamon/setup_cinnamon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Make links to all cinnamon-related files and folders
+ln -srf ./calendar@cinnamon.org        ~/.cinnamon/configs/calendar@cinnamon.org
+ln -srf ./menu@cinnamon.org            ~/.cinnamon/configs/menu@cinnamon.org
+ln -srf ./show-desktop@cinnamon.org    ~/.cinnamon/configs/show-desktop@cinnamon.org
+ln -srf ./notifications@cinnamon.org   ~/.cinnamon/configs/notifications@cinnamon.org
+ln -srf ./sound@cinnamon.org           ~/.cinnamon/configs/sound@cinnamon.org
+ln -srf ./panel-launchers@cinnamon.org ~/.cinnamon/configs/panel-launchers@cinnamon.org
+ln -srf ./user@cinnamon.org            ~/.cinnamon/configs/user@cinnamon.org
+ln -srf ./power@cinnamon.org           ~/.cinnamon/configs/power@cinnamon.org
+ln -srf ./window-list@cinnamon.org     ~/.cinnamon/configs/window-list@cinnamon.org
+
