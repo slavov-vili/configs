ec1440211ce29355b69762cda2020c68d71e368b
vilskin <slavov.vili@gmail.com>
2018-07-21 23:17:22 +0200

    Added ideavimrc

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -6,9 +6,11 @@ DIR=$(pwd)
 # remove the files in case they already exist
 rm ~/.vimrc
 rm ~/.gvimrc
+rm ~/.ideavimrc
 rm -r ~/.vim
 
 # Make links to all vim-related files and folders
 ln -sf $DIR/vimrc     ~/.vimrc
 ln -sf $DIR/gvimrc    ~/.gvimrc
+ln -sf $DIR/ideavimrc ~/.ideavimrc
 ln -sf $DIR/vim       ~/.vim
