5190ba5b9b1adbc75fc1ba16cada56a944712e66
vilskin <slavov.vili@gmail.com>
2018-08-24 13:29:40 +0200

    Minor changes

--- synergy/setup_synergy.sh
+++ synergy/setup_synergy.sh
@@ -4,7 +4,7 @@
 DIR=$(pwd)
 
 # remove the file in case it already exists
-rm ~/.config/synergy/synergy.conf
+rm ~/.synergy.conf
 
 # Make a link to synergy.conf
-ln -sf $DIR/synergy.conf ~/.config/synergy/synergy.conf
+ln -sf $DIR/synergy.conf ~/.synergy.conf
