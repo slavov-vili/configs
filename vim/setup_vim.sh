43e1c315a05c244edf1f77bd2db51c301078f20c
vilskin <slavov.vili@gmail.com>
2019-09-07 10:09:32 +0200

    Switched from "pathogen" to "vim-plug" and added some more plugins

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -14,3 +14,7 @@ ln -sf $DIR/vimrc     ~/.vimrc
 ln -sf $DIR/gvimrc    ~/.gvimrc
 ln -sf $DIR/ideavimrc ~/.ideavimrc
 ln -sf $DIR/vim       ~/.vim
+
+# "Install" vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
