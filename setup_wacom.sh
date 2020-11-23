6f19d38ebcfbadee6f5ca9da9190261b7813cd56
vilskin <slavov.vili@gmail.com>
2020-11-23 08:25:42 +0100

    Improved alias for xdg-open, added script to setup wacom tablet

---
+++ setup_wacom.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xsetwacom --set "Wacom Bamboo Connect Pen stylus" "Mode" "Relative"
+xsetwacom --set "Wacom Bamboo Connect Pen stylus" "CursorProximity" 15
+xsetwacom --set "Wacom Bamboo Connect Pen stylus" "Area" 0 0 42688 26680
