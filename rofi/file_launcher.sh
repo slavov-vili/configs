8e661b5353ce2e5270d49d222de3f025a5489325
vilskin <slavov.vili@gmail.com>
2021-01-22 17:20:39 +0100

    Rofi: Split config and added file launcher

---
+++ rofi/file_launcher.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rofi -theme ~/.config/rofi/file_launcher.rasi -show file-browser
--- rofi/setup.sh
+++ rofi/setup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 DIR=$(pwd)
+ROFI_HOME="~/.config/rofi"
 
-# Make links to the various scripts and the corresponding themes
+# Main config
+ln -svf $DIR/config.rasi ~/.config/rofi/config.rasi
+ln -svf $DIR/keybinds.rasi ~/.config/rofi/keybinds.rasi
+
+# Various themes
 ln -svf $DIR/app_launcher.rasi ~/.config/rofi/app_launcher.rasi
 ln -svf $DIR/window_switcher.rasi ~/.config/rofi/window_switcher.rasi
+ln -svf $DIR/file_launcher.rasi ~/.config/rofi/file_launcher.rasi
 
+# Launch scripts
 ln -svf $DIR/app_launcher.sh ~/.config/rofi/app_launcher.sh
 ln -svf $DIR/window_switcher.sh ~/.config/rofi/window_switcher.sh
+ln -svf $DIR/file_launcher.sh ~/.config/rofi/file_launcher.sh
 
+# Color themes
+ln -svnFf $DIR/themes ~/.config/rofi/themes
