411430a45e1d5a46f63e2db000769a70c3215d87
vilskin <slavov.vili@gmail.com>
2021-09-21 13:37:43 +0200

    [rofi] prompt looks more centered now and script now only requires 1 argument and outputs result

--- rofi/scripts/prompt.sh
+++ rofi/scripts/prompt.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $(realpath $0) [MESSAGE] [COMMAND]"
+if [ $# -eq 0 ]; then
+    echo "Usage: $(realpath $0) MESSAGE [COMMAND]"
     exit 1
 fi
 
 
-RESULT=$( echo -en "No\nYes\n" | rofi -theme  $HOME/.config/rofi/widgets/prompt.rasi -dmenu -p "$1")
+RESULT=$( echo -en "No\nYes\n" | rofi -theme  $HOME/.config/rofi/widgets/prompt.rasi -dmenu -format i -p "$1")
 
-if [ $RESULT == "Yes" ]; then
+if [ $RESULT -eq 1 ] && [ -n "$2" ]; then
     eval $2
 fi
+
+echo $RESULT
