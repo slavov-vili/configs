df9676e8a15ab99c773b612f74c8a787d4c7a87d
vilskin <slavov.vili@gmail.com>
2026-07-12 23:01:08 +0200

    VIM: Fix error of nvim-tree not toggling (call setup function earlier) - Also add other i3 workspaces to polybar

--- vim/plugin-config/file-tree.lua
+++ vim/plugin-config/file-tree.lua
@@ -1,3 +1,44 @@
+local function on_attach(bufnr)
+  local api = require("nvim-tree.api")
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.map.on_attach.default(bufnr)
+
+  -- custom mappings
+  vim.keymap.set("n", "?",     api.tree.toggle_help,                  opts("Help"))
+end
+
+require("nvim-tree").setup({
+  on_attach = on_attach,
+  view = {
+    side = "left",
+    width = 30,
+  },
+  update_focused_file = {
+    enable = true,
+    update_root = false,
+  },
+  hijack_directories = {
+    enable = true
+  },
+  modified = {
+    enable = true
+  },
+  diagnostics = {
+    enable = true
+  },
+  tab = {
+    sync = {
+      open = true,
+      close = true
+    }
+  },
+})
+
 local api = require("nvim-tree.api")
 
 local PREFIX = "<leader>t"
@@ -5,6 +46,8 @@ local function keymap(key, action)
     vim.keymap.set("n", PREFIX..key, action, { noremap = true, silent = true })
 end
 
+-- TODO: add custom mappings for tree (help)
+-- TODO: add any other useful functions
 -- Tree management
 -- FIXME: Find current file?
 keymap("t", api.tree.toggle)
@@ -13,9 +56,9 @@ keymap("o", api.tree.open)
 keymap("q", api.tree.close)
 keymap("r", api.tree.reload)
 -- Navigation
-keymap("f", api.tree.focus)
+keymap("F", api.tree.focus)
 -- FIXME: switch to tree?
-keymap("F", api.tree.find_file)
+keymap("f", api.tree.find_file)
 keymap("l", api.tree.collapse_all)
 keymap("L", api.tree.expand_all)
 -- Filters
@@ -24,6 +67,3 @@ keymap("g", api.tree.toggle_git_clean_filter)
 keymap("G", api.tree.toggle_gitignore_filter)
 keymap("x", api.tree.toggle_no_buffer_filter)
 
-
-
-require("nvim-tree").setup()
